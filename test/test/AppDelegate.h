0dace06f77283d9d2fcf9c1315bed806dc48c2e4
iKevinCai <ikevincai@163.com>
2015-11-04 23:36:31 +0800

    My First iOS APP

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by KevinCai on 15/11/4.
+//  Copyright © 2015年 KevinCai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
